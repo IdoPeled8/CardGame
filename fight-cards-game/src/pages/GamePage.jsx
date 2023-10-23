6f579b58bc62b073590a62aec048bd54bdeff16b
ido peled <Idop9081@gmail.com>
2023-10-23 23:23:39 +0300

    add to dos

--- fight-cards-game/src/pages/GamePage.jsx
+++ fight-cards-game/src/pages/GamePage.jsx
@@ -15,6 +15,10 @@ import { useEffect, useState } from "react";
 // TO DO
 // add save card for later attack logic
 // add visuals + looking + custom CSS
+// make authorization for admins to delete players and more...
+// make rooms so every table will be with limited players
+// make ready button so when all players ready start the game
+// if player join in the middle of the game put him in waiting list
 // add a chat for the game
 
 
