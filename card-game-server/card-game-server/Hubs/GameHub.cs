2920183b8f16ed69631ee2a9de2b0971d91d4d17
ido peled <Idop9081@gmail.com>
2023-10-25 10:08:39 +0300

    add full logic to accumulate + adding message sender in the hub

--- card-game-server/card-game-server/Hubs/GameHub.cs
+++ card-game-server/card-game-server/Hubs/GameHub.cs
@@ -21,6 +21,7 @@ namespace card_game_server.Hubs
             _gameData = gameData;
         }
 
+        //the context.id is not stable - need to change it to somthing better like identity\tokens or stuff like this
         // make better implements for duplicate code
 
         public async Task SendMessage(string message)
@@ -38,12 +39,11 @@ namespace card_game_server.Hubs
 
                 await Groups.AddToGroupAsync(Context.ConnectionId, "GameGroup");
 
-                var sender = _playersLogic.GetPlayerById(Context.ConnectionId);
+                var sender = _playersLogic.FindPlayerById(Context.ConnectionId);
 
                 await Clients.Caller.SendAsync("GetClientSender", sender);
 
-                //this is only for checks (dont really need it)
-                await SendMessage($"{name} has joined the game!");
+                await SendMessage($"{name} has joined the table!");
             }
             catch (Exception)
             {
@@ -56,59 +56,75 @@ namespace card_game_server.Hubs
         {
             if (_playersLogic.GetAllPlayers().Count <= 1)
             {
-                throw new ArgumentException("you dont have enoght players to start the game");
+                await Console.Out.WriteLineAsync("you dont have enoght players to start the game");
+                return;
             }
             _gameLogic.ClearPlayerData();
             _deckLogic.CreateNewDeck();
-
             _deckLogic.ShuffleDeck();
-
             _gameLogic.DealCards();
             var startPlayer = _gameLogic.WhoStart();
 
+            await SendMessage($"game started!!");
             await UpdateData(new Card(null!, 0, "noHealth.png"), startPlayer);
 
         } //if the code is testable this means hes good
 
-        public async Task AttackPlayer(string playerToAttackId, string playerTurnId)
-        {
-            var check = _playersLogic.CheckAuthorization(playerTurnId, Context.ConnectionId);
-            if (check)
-            {
-                //throw new Exception("not this client turn");
 
+        //there is a bugggg
+        public async Task AttackPlayer(string playerToAttackId, string currentPlayerTurnId)
+        {
+            //do this for all methods and also dont send the id to the logic just send the player
+            var currentPlayerTurn = _playersLogic.FindPlayerById(Context.ConnectionId);
+            if (currentPlayerTurn != null)
+            {
                 var card = _deckLogic.TakeCardFromDeck();
 
-                _playersLogic.AttackPlayer(playerToAttackId, card);
+                //i can send the attacker directly and not the id and serch again inside the logic
+                var playerToAttack = _playersLogic.AttackPlayer(playerToAttackId, card, currentPlayerTurn);
 
-                var playerTurn = _gameLogic.ChangeTurn();
+                //need to add here the accumulate card if have
+                await SendMessage($"{currentPlayerTurn.Name} just attacked {playerToAttack.Name} with:{card.Value}");
+
+                var NewplayerTurn = _gameLogic.ChangeTurn();
 
                 _gameLogic.CheckWinner();
 
-                await UpdateData(card, playerTurn);
+                await UpdateData(card, NewplayerTurn);
             }
 
         }
 
         public async Task ChangeGuard(string playerId)
         {
-            var card = _deckLogic.TakeCardFromDeck();
+            var currentPlayerTurn = _playersLogic.FindPlayerById(Context.ConnectionId);
+            if (currentPlayerTurn != null)
+            {
+                var card = _deckLogic.TakeCardFromDeck();
 
-            _playersLogic.ChangeGuard(playerId, card);
+                var playerToChange = _playersLogic.ChangeGuard(playerId, card);
 
-            var playerTurn = _gameLogic.ChangeTurn();
+                var NewPlayerTurn = _gameLogic.ChangeTurn();
 
-            await UpdateData(card, playerTurn);
+                await SendMessage($"{currentPlayerTurn.Name} just change guard to {playerToChange.Name} with:{card.Value}");
+                await UpdateData(card, NewPlayerTurn);
+            }
         }
 
         public async Task AccumulateCard(string accumulatePlayerId)
         {
-            var card = _deckLogic.TakeCardFromDeck();
+            var currentPlayerTurn = _playersLogic.FindPlayerById(Context.ConnectionId);
+            if (currentPlayerTurn != null)
+            {
+                var card = _deckLogic.TakeCardFromDeck();
 
-            _playersLogic.AccumulateCard(accumulatePlayerId, card);
-            var playerTurn = _gameLogic.ChangeTurn();
+                _playersLogic.AccumulateCard(currentPlayerTurn, card);
 
-            await UpdateData(card, playerTurn);
+                var NewPlayerTurn = _gameLogic.ChangeTurn();
+
+                await SendMessage($"{currentPlayerTurn.Name} just accumulate :|");
+                await UpdateData(card, NewPlayerTurn);
+            }
         }
 
 
@@ -129,6 +145,7 @@ namespace card_game_server.Hubs
 
         public async Task UpdateData(Card card, Player playerTurn)
         {
+            await Console.Out.WriteLineAsync(Context.ConnectionId);
             //change all to group
             _gameLogic.fillGamedata(card, playerTurn, _playersLogic.GetAllPlayers());
             await Clients.All.SendAsync("AfterMoveUpdate", _gameData);
--- card-game-server/card-game-server/Logic/PlayersLogic.cs
+++ card-game-server/card-game-server/Logic/PlayersLogic.cs
@@ -5,7 +5,9 @@ using card_game_server.Repositories;
 namespace card_game_server.Logic
 {
 
-
+    //fix all this duplicate new card
+    //seperate the attack logic to private logics for move stable and understandeble
+    //move all the moves logic to the game logic??
     public class PlayersLogic : IPlayersLogic
     {
         private readonly SimpleData _simpleData;
@@ -18,11 +20,6 @@ namespace card_game_server.Logic
         }
         public List<Player> GetAllPlayers() => _simpleData.Players;
 
-        //public Player CreatePlayer(string name)
-        //{
-        //    _simpleData.Players.Add(new Player(name,null));
-        //    return _simpleData.Players[_simpleData.Players.Count - 1];
-        //}
         public Player FindPlayerById(string playerId)
         {
             var player = _simpleData.Players.FirstOrDefault(player => player.Id.ToString() == playerId);
@@ -40,6 +37,7 @@ namespace card_game_server.Logic
         }
 
         public void RemoveAllPlayers() => _simpleData.Players.Clear();
+
         public void RemovePlayer(string id)
         {
             var player = _simpleData.Players.FirstOrDefault(player => player.Id.ToString() == id);
@@ -52,23 +50,26 @@ namespace card_game_server.Logic
                 throw new InvalidDataException("id donset exist");
             }
         }
-
-
         //attack and change guard should be in game logic?
-        public Player AttackPlayer(string playerToAttackId, Card attackCard)
+
+        public Player AttackPlayer(string playerToAttackId, Card attackCard, Player attacker) // can make this void? (async and return task?)
         {
             var playerToAttack = FindPlayerById(playerToAttackId);
 
             if (attackCard.Value <= playerToAttack.Hand[HandKeys.Guard]!.Value)
             {
-                Console.WriteLine("enemy guard is higher");
-                return playerToAttack;
+                if (attacker.Hand[HandKeys.Accumulate]!.Value <= playerToAttack.Hand[HandKeys.Guard]!.Value)
+                {
+                    Console.WriteLine("enemy guard is higher");
+                    //fix this duplicate
+                    attacker.Hand[HandKeys.Accumulate] = new Card(null!, 0, "noHealth.png");
+                    var index = _simpleData.Players.FindIndex(player => player.Id == attacker.Id);
+                    _simpleData.Players[index] = attacker;
+                    return playerToAttack;
+                }
             }
-
             var totalHealth = playerToAttack.Hand[HandKeys.Heart1]!.Value + playerToAttack.Hand[HandKeys.Heart2]!.Value;
-
-            var attackValue = attackCard.Value - playerToAttack.Hand[HandKeys.Guard]!.Value;
-
+            var attackValue = attackCard.Value + attacker.Hand[HandKeys.Accumulate]!.Value - playerToAttack.Hand[HandKeys.Guard]!.Value;
             totalHealth -= attackValue;
 
             if (totalHealth <= 0)
@@ -89,9 +90,16 @@ namespace card_game_server.Logic
                 totalHealth -= 13;
                 playerToAttack.Hand[HandKeys.Heart2] = _deckLogic.FindCardByValue(totalHealth);
             }
-            var index = _simpleData.Players.FindIndex(player => player.Id == playerToAttack.Id);
-            _simpleData.Players[index] = playerToAttack;
-            return _simpleData.Players[index];
+
+            playerToAttack.Hand[HandKeys.Accumulate] = new Card(null!, 0, "noHealth.png");
+            attacker.Hand[HandKeys.Accumulate] = new Card(null!, 0, "noHealth.png");
+
+            var indexToAttack = _simpleData.Players.FindIndex(player => player.Id == playerToAttack.Id);
+            var indexAttacker = _simpleData.Players.FindIndex(player => player.Id == attacker.Id);
+            _simpleData.Players[indexToAttack] = playerToAttack;
+            _simpleData.Players[indexAttacker] = attacker;
+
+            return _simpleData.Players[indexToAttack];
         }
 
         public Player ChangeGuard(string playerId, Card card)
@@ -108,32 +116,17 @@ namespace card_game_server.Logic
         public bool CheckAuthorization(string playerTurnId, string userId)
         {
             if (userId != playerTurnId)
-            {
-                Console.WriteLine("not client turn");
                 return false;
-            }
             else
-            {
-                Console.WriteLine("client is player");
                 return true;
-            }
 
         }
 
-        public Player GetPlayerById(string id)
+        public Player AccumulateCard(Player accumulatePlayer, Card card)
         {
-            // this method already exist so delete this when all working
-            var sender = _simpleData.Players.FirstOrDefault(player => player.Id == id);
-            return sender;
-        }
-
-        public Player AccumulateCard(string accumulatePlayerId, Card card)
-        {
-            var accumulatePlayer = FindPlayerById(accumulatePlayerId);
-
-            if (accumulatePlayer.Hand[HandKeys.Accumulate].Value != 0)
+            if (accumulatePlayer.Hand[HandKeys.Accumulate]!.Value != 0)
             {
-                Console.WriteLine("this player already have card");
+                Console.WriteLine("this player already have accumulate");
                 return accumulatePlayer;
                 // dont need to send him
             }
@@ -143,11 +136,6 @@ namespace card_game_server.Logic
             var index = _simpleData.Players.FindIndex(player => player.Id == accumulatePlayer.Id);
             _simpleData.Players[index] = accumulatePlayer;
             return accumulatePlayer;
-
-            /* when accumulate card save the card value and shape but make the photo (accumulate)
-             * change in the attack logic - if the attacker have accumulate card check him to and use it if can
-             * change when attack you - if somone that got attacked have accumulate card, remove his accumulate
-             */
         }
     }
 
