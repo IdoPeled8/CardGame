98cc9059e91eb6696730a2a5e7c1cee623e0f963
ido peled <Idop9081@gmail.com>
2023-11-02 10:12:39 +0200

    change root path

--- fight-cards-game/src/components/card/Card.jsx
+++ fight-cards-game/src/components/card/Card.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 
 const Card = ({imageName}) => {
-  const imageUrl = `../../../public/cards-deck-photos/${imageName}`
+  const imageUrl = `/public/cards-deck-photos/${imageName}`
   console.log(imageUrl)
   return <img className='card' src={imageUrl} alt="playing card" />
 };
